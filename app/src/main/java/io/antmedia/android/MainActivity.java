c885ad03aa1753a2f8d6660bcc6189e3d6418e85
HanSolCho <whgksthf702@naver.com>
2019-07-26 20:18:20 +0900

    fitst commit

---
+++ app/src/main/java/io/antmedia/android/MainActivity.java
@@ -0,0 +1,34 @@
+package io.antmedia.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import io.antmedia.android.liveVideoBroadcaster.*;
+import io.antmedia.android.liveVideoPlayer.LiveVideoPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * PLEASE WRITE RTMP BASE URL of the your RTMP SERVER.
+     */
+    public static final String RTMP_BASE_URL = "rtmp://cho1719.vps.phps.kr/LiveApp/";
+//    public static final String RTMP_BASE_URL = "rtmp://115.71.237.207/LiveApp/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(io.antmedia.android.liveVideoBroadcaster.R.layout.activity_main);
+    }
+
+    public void openVideoBroadcaster(View view) {
+        Intent i = new Intent(this, LiveVideoBroadcasterActivity.class);
+        startActivity(i);
+    }
+
+    public void openVideoPlayer(View view) {
+        Intent i = new Intent(this, LiveVideoPlayerActivity.class);
+        startActivity(i);
+    }
+}
